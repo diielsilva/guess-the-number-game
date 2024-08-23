7a149c6881b4fda8c70d9ed9163b62961e5520c4
Daniel Silva <diielsilvasousa@gmail.com>
2024-08-23 15:04:55 -0300

    chore: initial backend commit

---
+++ backend/guess-the-number/src/test/java/com/ddev/guess_the_number/GuessTheNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddev.guess_the_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessTheNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
